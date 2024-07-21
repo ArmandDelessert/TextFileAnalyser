c00a5d5c9d6513d58dc64226b809c91682c5d6e0
Armand Delessert <armand.delessert@objectis.ch>
2024-07-21 22:28:01 +0200

    Add classes `File` & `FileSystemContainer`

---
+++ TextFileAnalyser/File.cs
@@ -0,0 +1,14 @@
+﻿namespace TextFileAnalyser
+{
+    internal record File
+    {
+        public required string Name { get; set; }
+        public required string Extension { get; set; }
+        public required string Path { get; set; }
+
+        public bool IsTextFile { get; set; }
+        public int SpaceCount { get; set; }
+        public int TabCount { get; set; }
+        public bool FinalEmptyLine { get; set; }
+    }
+}
---
+++ TextFileAnalyser/FileSystemContainer.cs
@@ -0,0 +1,24 @@
+﻿namespace TextFileAnalyser
+{
+    internal class FileSystemContainer
+    {
+        public FileSystemContainer(string fullPath)
+        {
+            Name = System.IO.Path.GetDirectoryName(fullPath) ?? "[Root]";
+            Path = Directory.GetParent(fullPath)?.FullName ?? string.Empty;
+
+            Files = [];
+            FileSystemContainers = [];
+        }
+
+        public required string Name { get; set; }
+
+        public required string Path { get; set; }
+
+        public string FullPath => System.IO.Path.Combine(Path, Name);
+
+        public List<File> Files { get; }
+
+        public List<FileSystemContainer> FileSystemContainers { get; }
+    }
+}
