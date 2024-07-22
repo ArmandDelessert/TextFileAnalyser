9fea19b997e8fe1459010065b460c148e698e200
Armand Delessert <armand.delessert@objectis.ch>
2024-07-22 07:55:04 +0200

    Add class `Characters`

---
+++ TextFileAnalyser/Characters.cs
@@ -0,0 +1,14 @@
+﻿namespace TextFileAnalyser
+{
+    internal static class Characters
+    {
+        public const char Space = ' ';
+        public const char Tabulation = '\t';
+        public const char CarriageReturn = '\r';
+        public const char LineFeed = '\n';
+
+        public const string CrLf = "\r\n";
+
+        public static string MultiSpaces(int spaceCount) => new(' ', spaceCount);
+    }
+}
