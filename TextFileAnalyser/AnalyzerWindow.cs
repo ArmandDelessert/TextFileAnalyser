8b37c5a561ea79bcff74733fc84e2e322d3b326c
Armand Delessert <armand.delessert@objectis.ch>
2024-08-09 11:14:59 +0200

    Add class `AnalyzerWindow`

---
+++ TextFileAnalyser/AnalyzerWindow.cs
@@ -0,0 +1,27 @@
+﻿namespace TextFileAnalyser
+{
+    internal class AnalyzerWindow(int windowSize)
+    {
+        public char[] Window { get; set; } = new char[windowSize];
+
+        public int WindowSize { get; set; } = windowSize;
+
+        public int WindowIndex { get; set; } = 0;
+
+        public void AddChar(char c)
+        {
+            Window[WindowIndex] = c;
+            MoveWindowIndex();
+        }
+
+        public char[] GetWindow() => Window;
+
+        private void MoveWindowIndex()
+        {
+            if (WindowIndex < WindowSize - 1)
+                WindowIndex++;
+            else
+                WindowIndex = 0;
+        }
+    }
+}
