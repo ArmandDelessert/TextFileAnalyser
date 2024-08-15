87a3c3d33281ad8659d38e0034767c9575f624a1
Armand Delessert <armand.delessert@objectis.ch>
2024-08-15 23:20:48 +0200

    Add method `RollingIndex.DumbIncrease`

--- TextFileAnalyser/RollingIndex.cs
+++ TextFileAnalyser/RollingIndex.cs
@@ -20,17 +20,56 @@ namespace TextFileAnalyser.Test
 
         public int Index { get; private set; }
 
+        public int UpRollingCount { get; private set; } = 0;
+
+        public int DownRollingCount { get; private set; } = 0;
+
         public void Increase(int increment = 1)
         {
+            DumbIncrease(increment);
+        }
+
+        private void DumbIncrease(int increment)
+        {
+            if (increment > 0)
+            {
+                while (increment > 0)
+                {
+                    Index++;
+                    increment--;
+
+                    if (Index > MaxIndex)
+                        Index = MinIndex;
+                }
+            }
+            else if (increment < 0)
+            {
+                while (increment < 0)
+                {
+                    Index--;
+                    increment++;
+
+                    if (Index < MinIndex)
+                        Index = MaxIndex;
+                }
+            }
+        }
+
+        private void SmartIncrease(int increment)
+        {
+            // TODO : Finish, test and validate.
+
             Index += increment;
 
             if (Index < MinIndex)
             {
-                Index = ;
+                Index = MinIndex; // TODO : Pas juste, à corriger.
+                DownRollingCount++; // TODO : On peut faire plusieurs tours.
             }
             if (Index > MaxIndex)
             {
-                Index = ;
+                Index = MinIndex; // TODO : Pas juste, à corriger.
+                UpRollingCount++; // TODO : On peut faire plusieurs tours.
             }
         }
     }
