fdacf783d9d5db7e0445a1a8b2806c7cced5e1b4
Armand Delessert <armand.delessert@objectis.ch>
2024-08-16 22:54:07 +0200

    Rename & add unit test in `AnalyserWindowTest`

--- TextFileAnalyser.Test/AnalyserWindowTest.cs
+++ TextFileAnalyser.Test/AnalyserWindowTest.cs
@@ -4,7 +4,7 @@
     public class AnalyserWindowTest
     {
         [TestMethod]
-        public void AddChar_Then_GetChar_And_GetWindow()
+        public void AddChar_Then_GetChar_Test()
         {
             // Prepare
             const int windowSize = 4;
@@ -28,10 +28,33 @@
             {
                 Assert.AreEqual(expectedChars[i], actualChars[i]);
             }
+        }
 
-            for (int i = 0; i < analyserWindow.GetWindow().Length; i++)
+        [TestMethod]
+        public void GetWindow_Test()
+        {
+            // Prepare
+            const int windowSize = 4;
+            var analyserWindow = new AnalyserWindow(windowSize);
+
+            const int testCharCount = 9;
+            char[] expectedChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+            IList<IList<char>> windows = [];
+
+            // Act
+            for (int i = 0; i < testCharCount; i++)
             {
-                Assert.AreEqual(expectedChars[i + 1], analyserWindow.GetWindow()[i]);
+                analyserWindow.AddChar(expectedChars[i]);
+                windows.Add(analyserWindow.GetWindow());
+            }
+
+            // Test
+            for (int i = 0; i < windows.Count; i++)
+            {
+                for (int j = 0; j < windows[i].Count; j++)
+                {
+                    Assert.AreEqual(expectedChars[j], windows[i][j]);
+                }
             }
         }
     }
