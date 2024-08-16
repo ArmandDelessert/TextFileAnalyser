346b97ad49499d8f3c039e76793c06de42510f0a
Armand Delessert <armand.delessert@objectis.ch>
2024-08-16 22:54:50 +0200

    Update method `AnalyserWindow.GetWindow()`

--- TextFileAnalyser/AnalyserWindow.cs
+++ TextFileAnalyser/AnalyserWindow.cs
@@ -6,6 +6,8 @@ namespace TextFileAnalyser
     {
         public char[] Window { get; private set; } = new char[windowSize];
 
+        public int CharAdded { get; private set; } = 0;
+
         private RollingIndex CurrentCharIndex { get; set; } = new RollingIndex(0, windowSize - 1);
 
         private int NextCharIndex => CurrentCharIndex.GetNextIndex();
@@ -13,12 +15,29 @@ namespace TextFileAnalyser
         public void AddChar(char c)
         {
             Window[NextCharIndex] = c;
+            CharAdded++;
             IncreaseWindowIndex();
         }
 
         public char GetChar(int index = 0) => Window[CurrentCharIndex.GetNextIndex(-index)];
 
-        public char[] GetWindow() => Window; // TODO : Formatter le tableau pour que l'index actuel soit en position 0.
+        public IList<char> GetWindow()
+        {
+            // cas 1 : Aucun char ajouté.
+            if (CharAdded == 0)
+                return [];
+
+            // Cas 1 : Tableau déjà ordré.
+            if (NextCharIndex == 0)
+                return Window;
+
+            // Cas 2 : Début du remplissage du tableau (sans réécriture).
+            if (CharAdded < Window.Length)
+                return Window.Take(CurrentCharIndex.Index + 1).ToList();
+
+            // Cas 3 : Remplissage du talbeau avec réécriture.
+            return Window.Skip(NextCharIndex).Take(windowSize).ToList();
+        }
 
         private void IncreaseWindowIndex() => CurrentCharIndex.Increase();
     }
