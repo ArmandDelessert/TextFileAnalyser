e0588789dc89c8510ab4f109d5ce9c249bf4e632
Armand Delessert <armand.delessert@objectis.ch>
2024-09-21 16:25:44 +0200

    Use a file-scoped namespace for `Program.cs`

--- TextFileAnalyser/Program.cs
+++ TextFileAnalyser/Program.cs
@@ -1,62 +1,61 @@
 ﻿using System.Reflection;
 
-namespace TextFileAnalyser
+namespace TextFileAnalyser;
+
+internal class Program
 {
-    internal class Program
+    internal static void Main(string[] args)
     {
-        internal static void Main(string[] args)
+        if (args.Length > 0)
         {
-            if (args.Length > 0)
+            StartCli(args);
+        }
+        else
+        {
+            try
             {
-                StartCli(args);
+                PrintHeader();
+                StartInteractiveCli();
             }
-            else
-            {
-                try
-                {
-                    PrintHeader();
-                    StartInteractiveCli();
-                }
 #if !DEBUG
-                catch (Exception e)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"A fatal error occurred and the program will close.{Environment.NewLine}Exception details:{Environment.NewLine}{e}");
-                    Console.ResetColor();
-                }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"A fatal error occurred and the program will close.{Environment.NewLine}Exception details:{Environment.NewLine}{e}");
+                Console.ResetColor();
+            }
 #endif
-                finally
-                {
-                    Console.WriteLine("Press any key to exit...");
-                    Console.ReadKey();
-                }
-            }
-        }
-
-        private static void PrintHeader()
-        {
-            Console.WriteLine("Text file analyser");
-            Console.WriteLine($"Version {Assembly.GetExecutingAssembly().GetName().Version}");
-        }
-
-        private static void StartCli(string[] args)
-        {
-            if (args.Length > 0 && Path.Exists(args[0]))
+            finally
             {
-                Analyser analyzer = new();
-                analyzer.AnalyzePath(args[0]);
-            }
-            else
-            {
-                Console.WriteLine("You need to provide the path of the file or directory to analyse.");
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
             }
         }
+    }
 
-        private static void StartInteractiveCli()
+    private static void PrintHeader()
+    {
+        Console.WriteLine("Text file analyser");
+        Console.WriteLine($"Version {Assembly.GetExecutingAssembly().GetName().Version}");
+    }
+
+    private static void StartCli(string[] args)
+    {
+        if (args.Length > 0 && Path.Exists(args[0]))
         {
-            var path = UserInterface.AskUserForPath();
             Analyser analyzer = new();
-            analyzer.AnalyzePath(path);
+            analyzer.AnalyzePath(args[0]);
         }
+        else
+        {
+            Console.WriteLine("You need to provide the path of the file or directory to analyse.");
+        }
+    }
+
+    private static void StartInteractiveCli()
+    {
+        var path = UserInterface.AskUserForPath();
+        Analyser analyzer = new();
+        analyzer.AnalyzePath(path);
     }
 }
