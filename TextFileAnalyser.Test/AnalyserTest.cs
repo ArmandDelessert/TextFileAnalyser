db76a0ba91233986b695a10719b8d442d6ffb983
Armand Delessert <armand.delessert@objectis.ch>
2024-09-29 15:42:37 +0200

    Update & add unit tests

--- TextFileAnalyser.Test/AnalyserTest.cs
+++ TextFileAnalyser.Test/AnalyserTest.cs
@@ -9,6 +9,7 @@ public class AnalyserTest
     [DataRow("",            0, 0, 0, 0, 0, false, 0, 0, 0, false, 0, 0, 0)]
     [DataRow(" ",           1, 1, 1, 0, 0, false, 0, 0, 0, false, 1, 1, 1)]
     [DataRow("\t",          1, 1, 0, 0, 1, false, 0, 0, 0, false, 1, 1, 1)]
+
     [DataRow("\r",          1, 2, 0, 0, 0, false, 1, 0, 0, false, 0, 2, 2)]
     [DataRow("\n",          1, 2, 0, 0, 0, false, 0, 1, 0, false, 0, 2, 2)]
     [DataRow("\r\n",        2, 2, 0, 0, 0, false, 0, 0, 1, false, 0, 2, 2)]
@@ -17,18 +18,18 @@ public class AnalyserTest
     [DataRow("a\r",         2, 2, 0, 0, 0, false, 1, 0, 0, false, 0, 1, 1)]
     [DataRow("a\n",         2, 2, 0, 0, 0, false, 0, 1, 0, false, 0, 1, 1)]
     [DataRow("a\r\n",       3, 2, 0, 0, 0, false, 0, 0, 1, false, 0, 1, 1)]
+
     [DataRow("a ",          2, 1, 1, 0, 0, false, 0, 0, 0, false, 1, 0, 0)]
     [DataRow("a \r",        3, 2, 1, 0, 0, false, 1, 0, 0, false, 1, 1, 1)]
     [DataRow("a \n",        3, 2, 1, 0, 0, false, 0, 1, 0, false, 1, 1, 1)]
     [DataRow("a \r\n",      4, 2, 1, 0, 0, false, 0, 0, 1, false, 1, 1, 1)]
-    [DataRow("a ",          2, 1, 1, 0, 0, false, 0, 0, 0, false, 1, 0, 0)]
+    [DataRow("a\t",         2, 1, 0, 0, 1, false, 0, 0, 0, false, 1, 0, 0)]
     [DataRow("a\t\r",       3, 2, 0, 0, 1, false, 1, 0, 0, false, 1, 1, 1)]
     [DataRow("a\t\n",       3, 2, 0, 0, 1, false, 0, 1, 0, false, 1, 1, 1)]
     [DataRow("a\t\r\n",     4, 2, 0, 0, 1, false, 0, 0, 1, false, 1, 1, 1)]
 
-    [DataRow(" \ra\r\t\r",  6, 4, 1, 0, 1, false, 3, 0, 0, false, 2, 3, 2)]
-    [DataRow("\t a\r",      4, 2, 1, 0, 1, true,  1, 0, 0, false, 0, 2, 1)] // TODO : Corriger `HasMixedSpaceAndTab`.
     [DataRow("\n\r",        2, 3, 0, 0, 0, false, 1, 1, 0, true,  0, 3, 3)]
+
     [DataRow(" \r",         2, 2, 1, 0, 0, false, 1, 0, 0, false, 1, 2, 2)]
     [DataRow(" \n",         2, 2, 1, 0, 0, false, 0, 1, 0, false, 1, 2, 2)]
     [DataRow(" \r\n",       3, 2, 1, 0, 0, false, 0, 0, 1, false, 1, 2, 2)]
@@ -36,7 +37,14 @@ public class AnalyserTest
     [DataRow("\t\n",        2, 2, 0, 0, 1, false, 0, 1, 0, false, 1, 2, 2)]
     [DataRow("\t\r\n",      3, 2, 0, 0, 1, false, 0, 0, 1, false, 1, 2, 2)]
 
+    [DataRow(" \ra\r\t\r",  6, 4, 1, 0, 1, false, 3, 0, 0, false, 2, 3, 2)]
+    [DataRow("\t a\r",      4, 2, 1, 0, 1, true,  1, 0, 0, false, 0, 2, 1)] // TODO : Corriger `HasMixedSpaceAndTab`.
+
+    [DataRow("{\r  a\r}\r", 8, 4, 2, 1, 0, false, 3, 0, 0, false, 0, 1, 1)]
+    [DataRow("{\r\ta\r}\r", 7, 4, 0, 0, 1, false, 3, 0, 0, false, 0, 1, 1)]
+
     [DataRow("ABCDEFGHIJKLMNOPQRSTUVWXYZ\r\nabcdefghijklmnopqrstuvwxyz\r\n0123456789\r\n", 68, 4, 0, 0, 0, false, 0, 0, 3, false, 0, 1, 1)]
+
     public void AnalyzeStreamCharByCharTest(
     string input,
     int expectedCharCount,
